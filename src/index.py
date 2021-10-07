c306c923ab6250808bb11d699d649b14c12b195f
Felipe Ignacio Ponce Canales <felipe.ponce2@mail.udp.cl>
2021-10-07 16:11:44 -0300

    Log in

---
+++ src/index.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+# ---------------------------------------------------------------------------------------------
+# # LOG IN
+
+# s = requests.Session()
+
+# payload = {'username': 'nada122@gmail.com', 'password': 'javi2602'}
+# url = 'https://w3.gamehost.cl/dologin.php'
+
+# r = s.post(url, data=payload)
+# # r =requests.post(url, data=payload)
+# inicio = BeautifulSoup(r.content, 'html.parser')
+
+# entrada = inicio.find(menuitemname="Client Details")
+# if entrada == None:
+#     print("Pass incorrecta")
+# else:
+#     print("dentrop")
+
+
+# ---------------------------------------------------------------------------------------------
